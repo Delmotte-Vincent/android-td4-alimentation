6be7cdf7a47251e390f0f1f32ef47f3d4aaa8a03
YannLeon <yann.leon@etu.unice.fr>
2020-04-16 11:36:13 +0200

    clean up

--- AndroidProject/app/src/main/java/com/main/exercice2/androidproject/Client/iButtonClickListener.java
+++
@@ -1,9 +0,0 @@
-package com.main.exercice2.androidproject.Client;
-
-import android.view.View;
-
-public interface iButtonClickListener {
-    void onButtonProfileClicked(View button);
-    void onButtonMapClicked(View button);
-    void onButtonSignalerClicked(View button);
-}
