b475e0f082abf5daf17c60fa963450e884633b4a
Vincent-Delmotte <vincent.delmotte@etu.unice.fr>
2020-04-19 13:14:15 +0200

    newsfeed

--- AndroidProject/app/src/main/java/com/main/exercice2/androidproject/Client/ClientAlertFragment.java
+++ AndroidProject/app/src/main/java/com/main/exercice2/androidproject/Client/ClientAlertFragment.java
@@ -4,14 +4,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.main.exercice2.androidproject.R;
 
 public class ClientAlertFragment extends Fragment {
+
+
 
     ClientAlertFragment(){}
 
@@ -19,6 +24,18 @@ public class ClientAlertFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.frag_alert_client,container,false);
+
+        String[] menuItems = {"post 1","post 2","post 3"};
+
+        ListView listView = (ListView) rootView.findViewById(R.id.newsFeed);
+        ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>(
+                getActivity(),
+                android.R.layout.simple_list_item_1,
+                menuItems
+        );
+
+        listView.setAdapter(listViewAdapter);
+
         return rootView;
     }
 }
---
+++ AndroidProject/app/src/main/java/com/main/exercice2/androidproject/Post.java
@@ -0,0 +1,9 @@
+package com.main.exercice2.androidproject;
+
+public class Post {
+    private String message;
+
+    public Post(String message) {
+        this.message = message;
+    }
+}
