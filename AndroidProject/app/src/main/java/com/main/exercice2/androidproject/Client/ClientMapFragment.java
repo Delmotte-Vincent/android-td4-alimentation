aa5fb4d9e13b670069907c2f17d4a0d5dc94bfd6
YannLeon <yann.leon@etu.unice.fr>
2020-04-30 19:44:05 +0200

    Début de la recherche de producteur

--- AndroidProject/app/src/main/java/com/main/exercice2/androidproject/Client/ClientMapFragment.java
+++ AndroidProject/app/src/main/java/com/main/exercice2/androidproject/Client/ClientMapFragment.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -25,14 +28,20 @@ import org.osmdroid.views.overlay.OverlayItem;
 import java.util.ArrayList;
 import java.util.Objects;
 
-public class ClientMapFragment extends Fragment {
+public class ClientMapFragment extends Fragment implements SearchView.OnQueryTextListener {
     private MapView map ;
+    ArrayList<OverlayItem> items;
+    SearchView searchView;
+    ListView listView;
     ClientMapFragment(){}
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.frag_map_client,container,false);
+        searchView = rootView.findViewById(R.id.search);
+        searchView.setOnQueryTextListener(this);
+        listView = rootView.findViewById(R.id.listSearch);
         Configuration.getInstance().load(getActivity().getApplicationContext(),
                 PreferenceManager.getDefaultSharedPreferences(getActivity().getApplicationContext()));
 
@@ -45,13 +54,18 @@ public class ClientMapFragment extends Fragment {
         mapController.setCenter(startPoint);
         mapController.setZoom(18.0);
 
-        ArrayList<OverlayItem> items = new ArrayList<>();
+        items = new ArrayList<>();
         OverlayItem home = new OverlayItem("home","rallo's home", new GeoPoint(43.65020,7.00517));
         Drawable m =home.getMarker(0);
         items.add(home);
 
 
         items.add(new OverlayItem("resto","delice de maman",new GeoPoint(43.64950,7.00517)));
+
+        /** Mise en place de l'adapteur pour l'array list**/
+
+        ArrayAdapter adapter = new ArrayAdapter(getContext(),android.R.layout.simple_list_item_1,items);
+        ((ListView)rootView.findViewById(R.id.listSearch)).setAdapter(adapter);
 
         ItemizedOverlayWithFocus<OverlayItem> mOverlay = new ItemizedOverlayWithFocus<OverlayItem>(getActivity().getApplicationContext(), items,
                 new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
@@ -69,7 +83,7 @@ public class ClientMapFragment extends Fragment {
         mOverlay.setFocusItemsOnTap(true);
         map.getOverlays().add(mOverlay);
 
-
+        listView.setVisibility(View.GONE);
         return rootView;
     }
 
@@ -87,4 +101,15 @@ public class ClientMapFragment extends Fragment {
     }
 
 
+    @Override
+    public boolean onQueryTextSubmit(String s) {
+        listView.setVisibility(View.GONE);
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String s) {
+        listView.setVisibility(View.VISIBLE);
+        return true;
+    }
 }
