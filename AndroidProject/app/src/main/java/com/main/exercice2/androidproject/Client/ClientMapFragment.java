ab3c5fc46ef14c4c5688b35701a29475c423245f
YannLeon <yann.leon@etu.unice.fr>
2020-05-01 17:22:48 +0200

    maintenant c'est joli quand on tape quelquechose dans la recherche

--- AndroidProject/app/src/main/java/com/main/exercice2/androidproject/Client/ClientMapFragment.java
+++ AndroidProject/app/src/main/java/com/main/exercice2/androidproject/Client/ClientMapFragment.java
@@ -14,6 +14,9 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.preference.PreferenceManager;
 
+import com.main.exercice2.androidproject.AlertType;
+import com.main.exercice2.androidproject.CommercantListAdapter;
+import com.main.exercice2.androidproject.CommercantObjet;
 import com.main.exercice2.androidproject.R;
 
 import org.osmdroid.api.IMapController;
@@ -31,6 +34,7 @@ import java.util.Objects;
 public class ClientMapFragment extends Fragment implements SearchView.OnQueryTextListener {
     private MapView map ;
     ArrayList<OverlayItem> items;
+    ArrayList<CommercantObjet> commercantObjetArrayList;
     SearchView searchView;
     ListView listView;
     ClientMapFragment(){}
@@ -55,16 +59,24 @@ public class ClientMapFragment extends Fragment implements SearchView.OnQueryTex
         mapController.setZoom(18.0);
 
         items = new ArrayList<>();
-        OverlayItem home = new OverlayItem("home","rallo's home", new GeoPoint(43.65020,7.00517));
-        Drawable m =home.getMarker(0);
-        items.add(home);
+        commercantObjetArrayList=new ArrayList<>();
+        CommercantObjet homeCom = new CommercantObjet("home","rallo's home", AlertType.DEFAULT,null,new GeoPoint(43.65020,7.00517));
+        CommercantObjet restoCom = new CommercantObjet("resto","delice de maman",AlertType.DEFAULT,null,new GeoPoint(43.64950,7.00517));
+        commercantObjetArrayList.add(homeCom);
+        commercantObjetArrayList.add(restoCom);
+        for(CommercantObjet c : commercantObjetArrayList){
+            items.add(new OverlayItem(c.getTitle(),c.getMessage(),c.getGeoPoint()));
+        }
+        //OverlayItem home = new OverlayItem("home","rallo's home", new GeoPoint(43.65020,7.00517));
+        //Drawable m =home.getMarker(0);
+        //items.add(home);
 
 
-        items.add(new OverlayItem("resto","delice de maman",new GeoPoint(43.64950,7.00517)));
+        //items.add(new OverlayItem("resto","delice de maman",new GeoPoint(43.64950,7.00517)));
 
         /** Mise en place de l'adapteur pour l'array list**/
 
-        ArrayAdapter adapter = new ArrayAdapter(getContext(),android.R.layout.simple_list_item_1,items);
+        ArrayAdapter adapter =new CommercantListAdapter(this.getContext(),commercantObjetArrayList);
         ((ListView)rootView.findViewById(R.id.listSearch)).setAdapter(adapter);
 
         ItemizedOverlayWithFocus<OverlayItem> mOverlay = new ItemizedOverlayWithFocus<OverlayItem>(getActivity().getApplicationContext(), items,
