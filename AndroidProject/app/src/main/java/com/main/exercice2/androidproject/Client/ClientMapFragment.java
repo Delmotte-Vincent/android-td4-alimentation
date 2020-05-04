137aa1b3948ec916fa802f1fed7b3a2027ef3dc2
FanlingGU <61821720+FanlingGU@users.noreply.github.com>
2020-05-05 02:40:57 +0800

    gps

--- AndroidProject/app/src/main/java/com/main/exercice2/androidproject/Client/ClientMapFragment.java
+++ AndroidProject/app/src/main/java/com/main/exercice2/androidproject/Client/ClientMapFragment.java
@@ -1,16 +1,25 @@
 package com.main.exercice2.androidproject.Client;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SearchView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.preference.PreferenceManager;
 
@@ -31,6 +40,9 @@ import org.osmdroid.views.overlay.OverlayItem;
 import java.util.ArrayList;
 import java.util.Objects;
 
+import static android.content.Context.LOCATION_SERVICE;
+import static androidx.constraintlayout.widget.Constraints.TAG;
+
 public class ClientMapFragment extends Fragment implements SearchView.OnQueryTextListener {
     private MapView map ;
     ArrayList<OverlayItem> items;
@@ -39,6 +51,8 @@ public class ClientMapFragment extends Fragment implements SearchView.OnQueryTex
     ListView listView;
     ArrayAdapter adapter;
     ClientMapFragment(){}
+    private LocationManager locationManager;
+    private Location currentLocation;
 
     @Nullable
     @Override
@@ -54,14 +68,16 @@ public class ClientMapFragment extends Fragment implements SearchView.OnQueryTex
         map= rootView.findViewById(R.id.map);
         map.setTileSource(TileSourceFactory.MAPNIK);
         map.setBuiltInZoomControls(true);
-        GeoPoint startPoint = new GeoPoint(43.6520,7.00517);
+        //GeoPoint startPoint = new GeoPoint(43.6520,7.00517);
+        GeoPoint startPoint=new GeoPoint(currentLocation.getAltitude(),currentLocation.getLongitude());
         IMapController mapController = map.getController();
         mapController.setCenter(startPoint);
         mapController.setZoom(18.0);
 
         items = new ArrayList<>();
         commercantObjetArrayList=new ArrayList<>();
-        CommercantObjet homeCom = new CommercantObjet("home","rallo's home", AlertType.DEFAULT,null,new GeoPoint(43.65020,7.00517));
+        //CommercantObjet homeCom = new CommercantObjet("home","rallo's home", AlertType.DEFAULT,null,new GeoPoint(43.65020,7.00517));
+        CommercantObjet homeCom = new CommercantObjet("home","rallo's home", AlertType.DEFAULT,null,new GeoPoint(currentLocation.getAltitude(),currentLocation.getLongitude()));
         CommercantObjet restoCom = new CommercantObjet("resto","delice de maman",AlertType.DEFAULT,null,new GeoPoint(43.64950,7.00517));
         commercantObjetArrayList.add(homeCom);
         commercantObjetArrayList.add(restoCom);
@@ -72,6 +88,38 @@ public class ClientMapFragment extends Fragment implements SearchView.OnQueryTex
         //Drawable m =home.getMarker(0);
         //items.add(home);
 
+        LocationManager locationManager = (LocationManager) (getActivity().getSystemService(LOCATION_SERVICE));
+        //check if GPS permission is already GRANTED
+        final boolean permissionGranted = ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+        Log.d(TAG, "permissionGranted = " + permissionGranted);
+        if(!permissionGranted){
+            LocationListener loclistener=new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    currentLocation = location;
+                }
+
+                @Override
+                public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                }
+
+                @Override
+                public void onProviderEnabled(String provider) {
+                        Log.d(TAG, provider+" sensor ON");
+                }
+
+                @Override
+                public void onProviderDisabled(String provider) {
+                    Log.d(TAG, provider+" sensor OFF");
+                }
+            };
+            currentLocation=locationManager.getLastKnownLocation(Context.LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 1, loclistener);
+        }else {
+            Log.d(TAG, "Permission NOT GRANTED  ! ");
+            currentLocation=locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        }
 
         //items.add(new OverlayItem("resto","delice de maman",new GeoPoint(43.64950,7.00517)));
 
@@ -99,6 +147,7 @@ public class ClientMapFragment extends Fragment implements SearchView.OnQueryTex
         listView.setVisibility(View.GONE);
         return rootView;
     }
+
 
 
     @Override
--- AndroidProject/app/src/main/java/com/main/exercice2/androidproject/Client/MainClient.java
+++ AndroidProject/app/src/main/java/com/main/exercice2/androidproject/Client/MainClient.java
@@ -181,6 +181,15 @@ public class MainClient extends AppCompatActivity implements IButtonCLickedListe
                 }
                 break;
             }
+            case REQUEST_GPS: {  //GPS FINE LOCATION only autorisation result code
+                if( grantResults.length > 0 &&  grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast toast = Toast.makeText(getApplicationContext(), "FINE authorisation Granted", Toast.LENGTH_LONG);
+                    toast.show();
+                    Log.d( TAG, "(only) FINE LOCATION permission Granted");
+                } else {
+                    Log.d( TAG, "(only) FINE LOCATION permission NOT Granted");
+                }
+            } break;
         }
     }
 
