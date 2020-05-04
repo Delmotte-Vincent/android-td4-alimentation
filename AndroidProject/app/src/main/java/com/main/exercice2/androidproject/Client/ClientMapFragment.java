b4bc38b325ac8a0c0ae955a445baa4448e7bde73
FanlingGU <61821720+FanlingGU@users.noreply.github.com>
2020-05-05 04:27:11 +0800

    corect a logic error

--- AndroidProject/app/src/main/java/com/main/exercice2/androidproject/Client/ClientMapFragment.java
+++ AndroidProject/app/src/main/java/com/main/exercice2/androidproject/Client/ClientMapFragment.java
@@ -2,12 +2,14 @@ package com.main.exercice2.androidproject.Client;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -44,20 +46,23 @@ import static android.content.Context.LOCATION_SERVICE;
 import static androidx.constraintlayout.widget.Constraints.TAG;
 
 public class ClientMapFragment extends Fragment implements SearchView.OnQueryTextListener {
-    private MapView map ;
+    private MapView map;
     ArrayList<OverlayItem> items;
     ArrayList<CommercantObjet> commercantObjetArrayList;
     SearchView searchView;
     ListView listView;
     ArrayAdapter adapter;
-    ClientMapFragment(){}
+
+    ClientMapFragment() {
+    }
+
     private LocationManager locationManager;
     private Location currentLocation;
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.frag_map_client,container,false);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable final ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.frag_map_client, container, false);
         searchView = rootView.findViewById(R.id.search);
         searchView.setOnQueryTextListener(this);
         listView = rootView.findViewById(R.id.listSearch);
@@ -65,35 +70,35 @@ public class ClientMapFragment extends Fragment implements SearchView.OnQueryTex
                 PreferenceManager.getDefaultSharedPreferences(getActivity().getApplicationContext()));
 
         //test
-        map= rootView.findViewById(R.id.map);
+        map = rootView.findViewById(R.id.map);
         map.setTileSource(TileSourceFactory.MAPNIK);
         map.setBuiltInZoomControls(true);
         //GeoPoint startPoint = new GeoPoint(43.6520,7.00517);
-        GeoPoint startPoint=new GeoPoint(currentLocation.getAltitude(),currentLocation.getLongitude());
+        GeoPoint startPoint = new GeoPoint(currentLocation.getAltitude(), currentLocation.getLongitude());
         IMapController mapController = map.getController();
         mapController.setCenter(startPoint);
         mapController.setZoom(18.0);
 
         items = new ArrayList<>();
-        commercantObjetArrayList=new ArrayList<>();
+        commercantObjetArrayList = new ArrayList<>();
         //CommercantObjet homeCom = new CommercantObjet("home","rallo's home", AlertType.DEFAULT,null,new GeoPoint(43.65020,7.00517));
-        CommercantObjet homeCom = new CommercantObjet("home","rallo's home", AlertType.DEFAULT,null,new GeoPoint(currentLocation.getAltitude(),currentLocation.getLongitude()));
-        CommercantObjet restoCom = new CommercantObjet("resto","delice de maman",AlertType.DEFAULT,null,new GeoPoint(43.64950,7.00517));
+        CommercantObjet homeCom = new CommercantObjet("home", "rallo's home", AlertType.DEFAULT, null, new GeoPoint(currentLocation.getAltitude(), currentLocation.getLongitude()));
+        CommercantObjet restoCom = new CommercantObjet("resto", "delice de maman", AlertType.DEFAULT, null, new GeoPoint(43.64950, 7.00517));
         commercantObjetArrayList.add(homeCom);
         commercantObjetArrayList.add(restoCom);
-        for(CommercantObjet c : commercantObjetArrayList){
-            items.add(new OverlayItem(c.getTitle(),c.getMessage(),c.getGeoPoint()));
+        for (CommercantObjet c : commercantObjetArrayList) {
+            items.add(new OverlayItem(c.getTitle(), c.getMessage(), c.getGeoPoint()));
         }
         //OverlayItem home = new OverlayItem("home","rallo's home", new GeoPoint(43.65020,7.00517));
         //Drawable m =home.getMarker(0);
         //items.add(home);
 
-        LocationManager locationManager = (LocationManager) (getActivity().getSystemService(LOCATION_SERVICE));
+        final LocationManager locationManager = (LocationManager) (getActivity().getSystemService(LOCATION_SERVICE));
         //check if GPS permission is already GRANTED
         final boolean permissionGranted = ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
         Log.d(TAG, "permissionGranted = " + permissionGranted);
-        if(!permissionGranted){
-            LocationListener loclistener=new LocationListener() {
+        if (permissionGranted) {
+            LocationListener loclistener = new LocationListener() {
                 @Override
                 public void onLocationChanged(Location location) {
                     currentLocation = location;
@@ -106,19 +111,41 @@ public class ClientMapFragment extends Fragment implements SearchView.OnQueryTex
 
                 @Override
                 public void onProviderEnabled(String provider) {
-                        Log.d(TAG, provider+" sensor ON");
+                    Log.d(TAG, provider + " sensor ON");
+                    if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        return;
+                    }
+                    currentLocation = locationManager.getLastKnownLocation(provider);
                 }
 
                 @Override
                 public void onProviderDisabled(String provider) {
                     Log.d(TAG, provider+" sensor OFF");
+                    Intent intent=new Intent();
+                    intent.setAction(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                    startActivityForResult(intent,400);
+                    return;
                 }
             };
             currentLocation=locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 1, loclistener);
         }else {
             Log.d(TAG, "Permission NOT GRANTED  ! ");
-            currentLocation=locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            if(locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)){
+                currentLocation=locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            }else {
+                Intent intent=new Intent();
+                intent.setAction(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivityForResult(intent,400);
+
+            }
         }
 
         //items.add(new OverlayItem("resto","delice de maman",new GeoPoint(43.64950,7.00517)));
