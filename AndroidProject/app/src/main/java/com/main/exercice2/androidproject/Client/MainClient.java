e9ee6b85539c699c7145ba74d00f84dd85023159
Vincent-Delmotte <vincent.delmotte@etu.unice.fr>
2020-05-10 12:19:41 +0200

    notification

--- AndroidProject/app/src/main/java/com/main/exercice2/androidproject/Client/MainClient.java
+++ AndroidProject/app/src/main/java/com/main/exercice2/androidproject/Client/MainClient.java
@@ -12,9 +12,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Color;
-import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
@@ -27,6 +25,7 @@ import android.widget.EditText;
 
 import android.widget.ImageView;
 
+import android.widget.RemoteViews;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -50,7 +49,6 @@ import com.main.exercice2.androidproject.R;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
-import java.util.ArrayList;
 import java.util.List;
 
 
@@ -178,34 +176,32 @@ public class MainClient extends AppCompatActivity implements IButtonCLickedListe
      * @param priority
      */
     private void sendNotificationOnChannel(String titre, String desc, String channelId, int priority) {
-        Intent activityIntent = new Intent(this, MainActivity.class);
-        //TODO trouver le moyen de mettre MainClient.class sans que ça plante
-        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, activityIntent, 0);
 
-        Intent broadcastIntent = new Intent (this, NotificationReceiver.class);
-        broadcastIntent.putExtra("toastMessage", desc);
-        PendingIntent actionIntent = PendingIntent.getBroadcast(this, 0, broadcastIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        Bitmap picture = this.picture;
-
-        NotificationCompat.Builder notification = new NotificationCompat.Builder(this, channelId)
-                .setSmallIcon(R.drawable.baseline_account_circle_black_18dp)
-                .setContentTitle(titre)
-                .setContentText(desc)
-                .setPriority(priority)
-                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
-                .setColor(Color.BLUE)
-                .setShowWhen(true)
-                .setContentIntent(contentIntent)
-                .setAutoCancel(true)
-                .setOnlyAlertOnce(true)
-                .addAction(R.mipmap.ic_launcher, "Toast", actionIntent);
+        NotificationCompat.Builder notification = new NotificationCompat.Builder(this, channelId);
+        RemoteViews expandedView;
 
         if (!this.defaultPicture) {
-            notification.setLargeIcon(picture)
-                    .setStyle(new NotificationCompat.DecoratedCustomViewStyle())
-                    .setStyle(new NotificationCompat.BigPictureStyle().bigPicture(picture).bigLargeIcon(null));
+            expandedView = new RemoteViews(getPackageName(), R.layout.notification_expanded_image);
+
+            expandedView.setImageViewBitmap(R.id.image_view_expanded, this.picture);
+            expandedView.setTextViewText(R.id.text_view_expanded_1, titre);
+            expandedView.setTextViewText(R.id.text_view_expanded_2, type);
         }
+
+        else {
+            expandedView = new RemoteViews(getPackageName(), R.layout.notification_expanded_no_image);
+
+            expandedView.setTextViewText(R.id.text_view_expanded_1, titre);
+            expandedView.setTextViewText(R.id.text_view_expanded_2, type);
+            expandedView.setTextViewText(R.id.text_view_expanded_3, desc);
+        }
+
+        notification.setSmallIcon(R.drawable.eat_it_white)
+                .setContentTitle(titre)
+                .setContentText(type)
+                .setColor(Color.BLUE)
+                .setCustomBigContentView(expandedView)
+                .setStyle(new NotificationCompat.DecoratedCustomViewStyle());
 
         Notification.getNotificationManager().notify(++notificationId, notification.build());
     }
